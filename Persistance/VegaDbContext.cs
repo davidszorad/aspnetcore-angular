01700ba1cb085c0dbd82be820d460e2873e6dabe
David Szorad <davidszorad@Davids-MacBook-Pro.local>
2017-09-09 20:44:27 +0200

    Created database

--- Persistance/VegaDbContext.cs
+++ Persistance/VegaDbContext.cs
@@ -1,9 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using vega.Models;
 
 namespace vega.Persistance
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Make { get; set; }  // EF will figure out that it need to create Model as well with navigation property in Make class
         
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
+        {
+            
+        }
     }
 }
