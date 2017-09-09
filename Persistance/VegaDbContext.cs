20804ec351eb562575d8eaeb6ea39963facc2edf
David Szorad <davidszorad@Davids-MacBook-Pro.local>
2017-09-09 22:02:51 +0200

    Created Resources (they are removing inverse properties to get rid of self reference loops)

--- Persistance/VegaDbContext.cs
+++ Persistance/VegaDbContext.cs
@@ -5,7 +5,7 @@ namespace vega.Persistance
 {
     public class VegaDbContext : DbContext
     {
-        public DbSet<Make> Make { get; set; }  // EF will figure out that it need to create Model as well with navigation property in Make class
+        public DbSet<Make> Makes { get; set; }  // EF will figure out that it need to create Model as well with navigation property in Make class
         
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
         {
