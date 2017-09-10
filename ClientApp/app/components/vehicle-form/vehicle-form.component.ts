fa4b8bcd9e85901c9bbc6ad1da4a1e9e85e1633c
David Szorad <davidszorad@Davids-MacBook-Pro.local>
2017-09-10 21:37:27 +0200

    Populated frontend with data from services

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,3 +1,5 @@
+import { FeatureService } from './../../services/feature.service';
+import { MakeService } from './../../services/make.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-form.component.css']
 })
 export class VehicleFormComponent implements OnInit {
+  makes: any[];
+  models: any[];
+  features: any[];
+  vehicle: any = {};
 
-  constructor() { }
+  constructor(
+    private makeService: MakeService,
+    private featureService: FeatureService) { }
 
   ngOnInit() {
+    this.makeService.getMakes().subscribe(makes => this.makes = makes);
+    this.featureService.getFeatures().subscribe(features => this.features = features);
   }
 
+  /*
+  async
+  ==============================
+  ngOnInit() {
+    this.makeService.getMakes().subscribe(makes => {
+      this.makes = makes;
+      console.log("MAKES", this.makes);
+    });
+  }
+  */
+
+  onMakeChange() {
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+    this.models = selectedMake ? selectedMake.models : [];
+  }
 }
