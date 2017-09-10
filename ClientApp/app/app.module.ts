cf5448dce98673700c81b73860fde04bfe843266
David Szorad <davidszorad@Davids-MacBook-Pro.local>
2017-09-10 21:37:08 +0200

    Created ng service - feature

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,13 +1,17 @@
-import { MakeService } from './services/make.service';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
+
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
+
+import { FeatureService } from './services/feature.service';
+import { MakeService } from './services/make.service';
 
 @NgModule({
     bootstrap: [ AppComponent ],
@@ -20,6 +24,7 @@ import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.com
         VehicleFormComponent
     ],
     imports: [
+        FormsModule,
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -31,6 +36,7 @@ import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.com
         ])
     ],
     providers: [
+        FeatureService,
         MakeService
     ]
 })
