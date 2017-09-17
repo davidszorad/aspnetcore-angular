12237a7bf6b26af1766edf6da66a7309c6664670
David Szorad <davidszorad@192.168.1.13>
2017-09-17 21:07:15 +0200

    Added Vehicle Add and Delete CRUD methods

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -21,6 +21,21 @@ namespace vega.Controllers
             this.context = context;
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleResource = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);
+        }
+        
         [HttpPost]
         public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
         {
@@ -56,6 +71,11 @@ namespace vega.Controllers
             }
             
             var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
             mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);  // vehicle is destination
             vehicle.LastUpdate = DateTime.Now;
 
@@ -64,6 +84,22 @@ namespace vega.Controllers
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
             return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
         }
     }
 }
